bae5770bb0a077cf6c6aee823e8820d3a126c238
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-05 15:53:54 -0800

    first commit

---
+++ project04.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num2 == 0:
+    print("ERROR: Division by zero is not allowed.")
+else:
+    quotient = num1/num2
+    print(quotient)
