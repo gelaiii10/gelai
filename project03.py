bbcecacf45a513bd5fc0c228871d03698b7e33bd
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-05 15:52:55 -0800

    first commit

---
+++ project03.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+difference = num1 - num2
+print(f"the difference between {num1} and   {num2} is:", difference)
